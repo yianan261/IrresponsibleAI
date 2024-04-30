6e95292d572faf0b299242a445579efcdc2bec76
yianan261 <yian.chen0131@gmail.com>
2024-04-30 11:32:23 -0700

    test with llm lingua

--- api/param_data.py
+++ api/param_data.py
@@ -1,4 +1,5 @@
 from .results import *
+from llmlingua import PromptCompressor
 
 
 def get_tools():
@@ -13,7 +14,7 @@ def get_tools():
                     "properties": {
                         "query": {
                             "type": "string",
-                            "description": "The question for the search engine to search for the city and state of the company. It should be a question, for example where is the headquarter of so-and-so company?",
+                            "description": "The question for the search engine to search for the city and state of the company. It should be a question, for example where is the headquarters of so-and-so company?",
                         },
                     },
                     "required": ["query"],
@@ -31,6 +32,11 @@ def get_messages(prompt, article_text, message_type="one"):
     :message_type: if "one" selected only one prompt. If "multi" selected there will be a multi-role conversation
     :return: message list for chat completions API.
     """
+    llm_lingua = PromptCompressor(
+        "TheBloke/Llama-2-7b-Chat-GPTQ", model_config={"revision": "main"}
+    )
+    compressed_article = llm_lingua.compress_prompt(article_text)
+    print("COMPRESSED ARTICLE====>>>", compressed_article)
     message = []
     if message_type == "one":
         message = [
@@ -45,7 +51,7 @@ def get_messages(prompt, article_text, message_type="one"):
                 "content": f"""
             Read this article and do the following: 
             ====start of article====
-            {article_text}
+            {compressed_article}
             ====end of article ====
             Find the city and state of the company in question in the article. You may use function calling.
             Return JSON response.
@@ -62,6 +68,10 @@ def update_messages_with_location(article_text, prompt, location_candidates):
     :param location_candidates: list of Google Custom Search API result for LLM to determine location of the company
     :return: message list for chat completions API
     """
+    llm_lingua = PromptCompressor(
+        "TheBloke/Llama-2-7b-Chat-GPTQ", model_config={"revision": "main"}
+    )
+    compressed_article = llm_lingua.compress_prompt(article_text)
     message = [
         {"role": "system", "content": "You are a helpful assistant."},
         {"role": "user", "content": prompt},
@@ -93,4 +103,35 @@ def update_messages_with_location(article_text, prompt, location_candidates):
             """,
         },
     ]
-    return message
+    message2 = [
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": prompt},
+        {
+            "role": "assistant",
+            "content": example_output_explanation_id_1,
+        },
+        {
+            "role": "user",
+            "content": "Check if your reasoning makes sense and is supported by the article text. Give your final classification result in JSON format",
+        },
+        {
+            "role": "assistant",
+            "content": example_output_id_1,
+        },
+        {
+            "role": "user",
+            "content": f"""
+            read this article: 
+            ====start of article====
+            {compressed_article}
+            ====end of article ====
+            please do the following:
+            request 1. Follow the previous example steps, provide classification and reasoning. Construct your final classification results in JSON format.
+            request 2. From this list of location candidates from Google Search API that returns search results on the city and state of the company, 
+            ```location candidates = {location_candidates}```
+            determine which candidate most-likely has the correct location of company in question in the article and note the city and state of the company of the chosen candidate.
+            Compare the `company city` and `company state` field results with your result from request 1; determine which `company city` and `company state` are the correct answers and update if necessary. Provide your final full classification result in JSON.
+            """,
+        },
+    ]
+    return message2
